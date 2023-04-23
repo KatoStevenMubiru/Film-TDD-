5030d44d82df0f03b5eeab24726e64692b00cf84
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-04-23 11:07:42 +0300

    Initial Commit

---
+++ Film(TDD)UITests/Film_TDD_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Film_TDD_UITestsLaunchTests.swift
+//  Film(TDD)UITests
+//
+//  Created by Kato Steven Mubiru on 23/04/2023.
+//
+
+import XCTest
+
+final class Film_TDD_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
