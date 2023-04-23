744549f1f87d27440c17d7955867315937d5db10
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-04-23 12:39:15 +0300

    Create movieManagerTests.swift

---
+++ Film(TDD)Tests/movieManagerTests.swift
@@ -0,0 +1,26 @@
+//
+//  movieManagerTests.swift
+//  Film(TDD)Tests
+//
+//  Created by Kato Steven Mubiru on 23/04/2023.
+//
+
+import XCTest
+
+@testable import Film_TDD_
+final class movieManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testInit_MoviesToSee_ReturnsZero(){
+        let sut = MovieManger()//sut is system under test
+        
+    }//testInit_MoviesToSee_ReturnsZero
+
+}
