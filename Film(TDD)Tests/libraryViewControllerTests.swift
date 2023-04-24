2c7338c7d46149617c0e7bb222068165b59cc713
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-04-24 17:47:15 +0300

    Create libraryViewControllerTests.swift

---
+++ Film(TDD)Tests/libraryViewControllerTests.swift
@@ -0,0 +1,26 @@
+//
+//  libraryViewControllerTests.swift
+//  Film(TDD)Tests
+//
+//  Created by Kato Steven Mubiru on 24/04/2023.
+//
+
+import XCTest
+
+@testable import Film_TDD_
+
+final class libraryViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+   //MARK: Nil Checks
+    func testLibraryVC_TableViewShouldNotBeNil(){
+        
+    }
+}
